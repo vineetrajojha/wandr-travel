59d9f375d5d83075dcbf3602d4ed5160c0d64b8a
Vineet Raj <vineetcres@gmail.com>
2026-04-29 01:24:26 +0530

    feat: auth

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -2,7 +2,7 @@ const supabaseUrl = 'https://vdzqejhtdrhzfhgxrvce.supabase.co';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZkenFlamh0ZHJoemZoZ3hydmNlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY3NTc3MDQsImV4cCI6MjA5MjMzMzcwNH0.XlbYP11T1o1oUCw6MrbWaRNZHC951VXDAXP-lZNfI4Y';
 
 // Initialize the Supabase client
-const supabase = window.supabase.createClient(supabaseUrl, supabaseAnonKey);
+const supabaseClientInstance = window.supabase.createClient(supabaseUrl, supabaseAnonKey);
 
 // Attach to global window object
-window.supabaseClient = supabase;
+window.supabaseClient = supabaseClientInstance;
